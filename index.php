7d191f5dcac7c1d2d7ca85f9d6ca736b121565cb
Kilian <kilian.moun@ynov.com>
2025-05-22 14:57:41 +0200

    Initial commit

--- index.php
+++ index.php
@@ -1,2 +1,14 @@
 <?php
-    echo "Hello, World!";
+    $mysqlClient = new PDO(
+        'mysql:host=localhost;dbname=jo;charset=utf8',
+        'root',
+        ''
+    );
+
+    $query = $mysqlClient->query('SELECT * FROM jo.`100`;');
+    $query->execute();
+
+    $data = $query->fetchAll(PDO::FETCH_ASSOC);
+
+    $mysqlClient = null;
+    $dbh = null;
