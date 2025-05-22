f7cfc3ca08bf691c8744aec69797b3f7301246de
Kilian <kilian.moun@ynov.com>
2025-05-22 16:00:16 +0200

    add bdd manager

--- index.php
+++ index.php
@@ -1,14 +1,34 @@
 <?php
-    $mysqlClient = new PDO(
-        'mysql:host=localhost;dbname=jo;charset=utf8',
-        'root',
-        ''
-    );
+include 'includes/loginDB.php';
+?>
 
-    $query = $mysqlClient->query('SELECT * FROM jo.`100`;');
-    $query->execute();
 
-    $data = $query->fetchAll(PDO::FETCH_ASSOC);
+<!DOCTYPE html>
+    <html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>JO 2024</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
 
-    $mysqlClient = null;
-    $dbh = null;
+    <body>
+        <main>
+            <section>
+                <h2>Résultats</h2>
+                <table>
+                    <tr>
+                        <th>Nom</th>
+                        <th>Temps</th>
+                    </tr>
+                    <?php $data = getNameAndTime();
+                    foreach ($data as $row) { ?>
+                        <tr>
+                            <td><?php echo $row['nom']; ?></td>
+                            <td><?php echo $row['temps']; ?></td>
+                        </tr>
+                    <?php } ?>
+                </table>
+            </section>
+        </main>
+    </body>
